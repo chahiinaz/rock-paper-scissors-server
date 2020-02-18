0f51ce8426bdb23653484914a40cf170d7935beb
chahiinaz <chahiinaz@gmail.com>
2020-02-18 10:30:02 +0100

    Create gameroom post route

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const port = process.env.PORT || 4000;
-const userRouter = require("./user/router");
 const app = express();
 
 const cors = require("cors");
@@ -11,6 +10,7 @@ const bodyParser = require("body-parser");
 const parserMiddleware = bodyParser.json();
 app.use(parserMiddleware);
 
+//Stream setup
 const Sse = require("json-sse");
 const stream = new Sse();
 
@@ -23,6 +23,12 @@ app.get("/stream", (req, res) => {
   stream.init(req, res);
 });
 
+//Insert gameroom factory into stream
+const gameroomFactory = require("./gameroom/router");
+const gameroomRouter = gameroomFactory(stream);
+app.use(gameroomRouter);
+
+const userRouter = require("./user/router");
 app.use(userRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
