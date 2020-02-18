b89575f658bf0ff413ee4314b0a185edb1918b68
chahiinaz <chahiinaz@gmail.com>
2020-02-18 10:19:55 +0100

    Initialize stream

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-
 const port = process.env.PORT || 4000;
-
 const userRouter = require("./user/router");
-
 const app = express();
 
+const cors = require("cors");
 const corsMiddleware = cors();
 app.use(corsMiddleware);
 
+const bodyParser = require("body-parser");
 const parserMiddleware = bodyParser.json();
 app.use(parserMiddleware);
+
+const Sse = require("json-sse");
+const stream = new Sse();
+
+app.get("/test", (req, res) => {
+  stream.send("test");
+  res.send("hello");
+});
+
+app.get("/stream", (req, res) => {
+  stream.init(req, res);
+});
 
 app.use(userRouter);
 
