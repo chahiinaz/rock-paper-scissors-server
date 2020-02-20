d87d20ea645449d2b78c673d37670f94858d2ba9
Entliczekperliczek <ewaperlinska55@gmail.com>
2020-02-20 15:55:56 +0100

    Fix router for game logic

--- gameroom/router.js
+++ gameroom/router.js
@@ -93,6 +93,33 @@ function factory(stream) {
     }
   });
 
+  router.put("/player/:choice", authMiddleware, async (req, res, next) => {
+    const player = req.player;
+
+    const gameroom = await Gameroom.findByPk(player.gameroomId);
+    const { choice } = req.params;
+
+    await player.update({ choice: choice });
+    const playersInGame = await Player.findAll({
+      where: { gameroomId: gameroom.id }
+    });
+
+    const chosen = playersInGame.every(player => player.choice);
+
+    if (chosen) {
+      const gameWinner = gameLogic(playersInGame);
+
+      if (gameWinner) {
+        const { winner } = gameWinner;
+        await winner.update({
+          points: winner.points + 1,
+          game_won: winner.game_won + 1
+        });
+      }
+      res.send({ chosen });
+    }
+  });
+
   return router;
 }
 
